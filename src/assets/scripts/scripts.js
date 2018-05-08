9a75e276303674f4d4376eec1b6d63d8b9f1ae8d
Leonid Zychkov <buldog52@gmail.com>
2018-05-09 00:29:56 +0300

    Added js code for showing modal forms

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -2,21 +2,6 @@ $(function() {
     $('.search__select select').jselector();
     $('.filter__selects select').jselector();
 
-    // $('.filter__innerTabs').each(function () {
-    //   var $liTab = $('.filter__innerTab', this);
-    //   var $liBlock = $('.filter__innerBlock');
-    //   console.log ($liTab);
-    //
-    //   $liTab.eq(1).addClass('filter__innerTab_active');
-    //   $liBlock.eq(1).addClass('filter__innerBlock_active');
-    //
-    //   $liTab.click(function () {
-    //     $liTab.eq($(this).index()).addClass('filter__innerTab_active').siblings().removeClass('filter__innerTab_active');
-    //     $liBlock.eq($(this).index()).addClass('filter__innerBlock_active').siblings().removeClass('filter__innerBlock_active');
-    //   })
-
-    })
-
 });
 
 // import $ from 'jquery';
