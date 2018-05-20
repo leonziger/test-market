dd0e08f7ebe8b2a1233a090d01ed37c1c403bf2e
Leonid Zychkov <buldog52@gmail.com>
2018-05-20 21:45:06 +0300

    Added dropdown menu, who became visible by click left-menu button

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -2,6 +2,12 @@ import $ from 'jquery';
 
 
 $('.left-menu__header').click(function () {
-  $(this).toggleClass('left-menu__header_active');
+  $('.left-menu__toggle').toggleClass('left-menu__toggle_active');
+  $('.menu__toggle').toggleClass('menu__toggle_active');
   $($(this).attr('data-target')).toggleClass('is-open');
+});
+
+$('.menu__toggle').click(function () {
+  $('.menu').toggleClass('is-open');
+  $('.left-menu__toggle').toggleClass('left-menu__toggle_active');
 });
