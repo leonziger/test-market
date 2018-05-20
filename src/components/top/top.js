c55caf0a9e97835a2aeaeb5a449404134f56d753
Leonid Zychkov <buldog52@gmail.com>
2018-05-20 22:19:36 +0300

    Finished to code left-menu drop-down block

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 
 $('.left-menu__header').click(function () {
   $('.left-menu__toggle').toggleClass('left-menu__toggle_active');
-  $('.menu__toggle').toggleClass('menu__toggle_active');
   $($(this).attr('data-target')).toggleClass('is-open');
 });
 
